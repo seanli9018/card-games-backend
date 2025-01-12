88059433db7b1efd28d5c95e731b0fc2bee41c14
seanli <seanli9018@gmail.com>
2025-01-12 13:39:39 -0600

    remove allowHeaders in corsOptions

--- src/api/index.ts
+++ src/api/index.ts
@@ -13,7 +13,6 @@ app.use(json());
 
 const corsOptions = {
   origin: ["http://localhost:3000", "https://card-games-ecru.vercel.app/"],
-  allowedHeaders: ["Content-Type", "Authorization"], // Add custom headers here
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
