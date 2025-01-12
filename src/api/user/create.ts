ca42176111d6eff2d415d41e7b3e727b109f94b9
seanli <seanli9018@gmail.com>
2025-01-11 19:43:28 -0600

    fixed ts build

--- src/api/user/create.ts
+++ src/api/user/create.ts
@@ -1,7 +1,8 @@
-import { from } from "../../lib/supabaseClient";
+// import { from } from "../../lib/supabaseClient";
+import type { Response, Request } from "express";
 
 // create user
-export default async (req, res) => {
+export default async (req: Request, res: Response) => {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
@@ -14,13 +15,13 @@ export default async (req, res) => {
   }
 
   // Insert into Supabase
-  const { data, error } = await from("user").insert([
-    { email, username, password },
-  ]);
+  // const { data, error } = await from("user").insert([
+  //   { email, username, password },
+  // ]);
 
-  if (error) {
-    return res.status(500).json({ error: error.message });
-  }
+  // if (error) {
+  //   return res.status(500).json({ error: error.message });
+  // }
 
-  res.status(201).json({ message: "User created successfully", user: data });
+  // res.status(201).json({ message: "User created successfully", user: data });
 };
